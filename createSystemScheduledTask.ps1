89647ae9f16d8e9805926170ccfdf0fca392f225
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2020-12-03 12:26:07 -0300

    feat: create a basic system task example

---
+++ createSystemScheduledTask.ps1
@@ -0,0 +1,18 @@
+$scriptToExecutePath = "C:\\script-name-here.ps1";
+$taskName = "MongoDB Backup"
+$taskDescription = "Make a dump of a MongoDB database"
+
+$action = New-ScheduledTaskAction `
+  -Execute 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe' `
+  -Argument "-NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass -File $scriptToExecutePath"
+  
+$trigger = New-ScheduledTaskTrigger -Daily -At 1am
+  
+$taskSettings = New-ScheduledTaskSettingsSet -Compatibility Win8
+
+Register-ScheduledTask `
+  -TaskName $taskName `
+  -Action $action `
+  -Trigger $trigger `
+  -Settings $taskSettings `
+  -Description $taskDescription
