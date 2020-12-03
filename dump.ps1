6ef88073eb72fc62740afd1dfa567256b383d103
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2020-12-02 21:04:31 -0300

    improve script comments

--- dump.ps1
+++ dump.ps1
@@ -1,32 +1,38 @@
-<#===  Set the MongoDB access variables  ===#>
+#region Variables
+
+<# Set the MongoDB access variables #>
 $databaseName = "databaseName"
 # $username = ""
 # $password = ""
 $mongoDbHost = "localhost:27017"
 
-<#===  Set the folder's location and name  ===#>
+<# Set the folder's location and name #>
 $backupPath = "C:\Path\To\Back\Folder"
 $currentDate = get-date -format yyyyMMddHHmm
 $directoryName = "$databaseName-$currentDate"
 $directoryPath = Join-Path $backupPath $directoryName
 
-<#===  Zip  ===#>
+<# Zip #>
 $zippedFileDestinationPath = "$backupPath\$directoryName.zip";
 
-<#===  Backup process  ===#>
-$Watch = New-Object System.Diagnostics.StopWatch
-$Watch.Start()
+#endregion
+
+#region Backup Process
+$watch = New-Object System.Diagnostics.StopWatch
+$watch.Start()
 Write-Host "Backing up the Database: '$databaseName' to local directory: $backupPath."
 
 # Use this command when the database require authorization
 # mongodump -h $mongoDbHost -d $databaseName -u $username -p $password -o "$directoryPath"
 
+# ATTENTION ⚠: Use the absolute path if you haven't added mongo to your System Path
+# Ex: C:\mongodb\bin\mongodump.exe -h $mongoDbHost -d $databaseName -o "$directoryPath"
 mongodump -h $mongoDbHost -d $databaseName -o "$directoryPath"
 
 Write-Host "Creating the backup for $databaseName..."
 
-$Watch.Stop();
-Write-Host "MongoDB backup completed in "$Watch.Elapsed.ToString()
+$watch.Stop();
+Write-Host "MongoDB backup completed in "$watch.Elapsed.ToString()
 
 # Zip sfolder
 Write-Host "Zipping the folder backup folder..."
@@ -35,3 +41,5 @@ Compress-Archive -Path "$directoryPath" -DestinationPath $zippedFileDestinationP
 # Delete the backup folter created
 Write-Host "Delete backup's folder"
 Remove-Item "$directoryPath" -Recurse
+
+#endregion
